4f52378df3781c58064d0fac3e6dc7e3bcab2e39
frank <frankjoserr0@gmail.com>
2023-11-10 12:26:35 -0400

    reproductor de musica con jv

---
+++ reproductor/datosbasicos.js
@@ -0,0 +1,20 @@
+let datoString = "hola mundo"
+let datoNumber = 45
+let datoBooleano = false
+
+let datoVacio = null
+let datoIndefinido = undefined
+
+let datoLista = [
+    "frank",
+    "josy",
+]
+
+let datoObjeto = {
+    username: "pudin123uwu",
+
+}
+
+
+console.log(datoLista);
+
