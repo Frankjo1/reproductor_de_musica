b8014febd478fecba6c4e542a8f37b01711f7960
frank <frankjoserr0@gmail.com>
2023-11-15 12:38:11 -0400

    avance con java al reproductor

---
+++ script.js
@@ -0,0 +1,30 @@
+let contenedor = document.querySelector('#track-list')
+console.log(contenedor)   
+
+
+axios.get('https://leonardoapi.onrender.com/music')
+    .then( (res) => {
+
+
+        res.data.map( (song) => {//  console.log(song)
+            
+            let div = document.createElement('div')
+            div.classList.add('musica')
+
+            div.innerHTML =`
+            <img class="album" src="${song.path.front}" alt="">
+            <div>
+                <h2 class="cancion">${song.title}</h2>
+                <p class="autores">${song.author}</p>
+            </div>`
+
+            div.addEventListener('click', () => {
+                console.log(`El usuario hizo click en ${song.title}`)
+            })
+
+            contenedor.appendChild(div)
+
+
+        } )
+
+    } )
